519d20393ac2d7a1286b52953539e98e7abe265b
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-30 14:20:27 -0500

    Creacion endpoint nombre producto

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.get("/productos" , (req,res)=>{
     </ol>`)
 });
 
+app.get("/productos/:nombre" , (req,res)=>{
+  const producto = req.params.nombre
+  res.send(`El producto es ${producto}`)
+})
+
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
