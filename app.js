5ccfc3966c55286dd28fe6320f91ad02b4877a60
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-30 13:40:41 -0500

    Creacion endpoint productos e importacion bodyparser

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import express from 'express'; // <--- Método ES (ESM)
 import {configDotenv} from "dotenv"
 configDotenv()
+import bodyParser from "body-parser";
 
 const app = express();
 const port= process.env.PORT || 3000;
 
+//Configurar el uso  de body.parser para la aplicacion
+app.use(express.json())
+app.use(express.urlencoded({extend: true}))
+
 app.get("/", (_, res) => {
   res.send('Hola , estamos aprendiendo express con la ficha 3407184');
+});
+
+app.get("/productos" , (req,res)=>{
+  res.send(`<h1>Listado de productos</h1>
+    <ol>
+    <li>Televisor</li>
+    <li>Celular</li>
+    <li>Impresora</li>
+    </ol>`)
 });
 
 app.listen(port, () => {
