c24f5196b73d3d773a02ab84872c5f2de4b072aa
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-30 15:19:44 -0500

    Creacion endpoint Saludo por nombre y error

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port= process.env.PORT || 3000;
 
 //Configurar el uso  de body.parser para la aplicacion
 app.use(express.json())
-app.use(express.urlencoded({extend: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (_, res) => {
   res.send('Hola , estamos aprendiendo express con la ficha 3407184');
@@ -27,6 +27,17 @@ app.get("/productos/:nombre" , (req,res)=>{
   const producto = req.params.nombre
   res.send(`El producto es ${producto}`)
 })
+
+app.get("/saludo/:nombre", (req, res) => {
+    const { nombre } = req.params;
+    // Si tiene menos de 3 letras, retorna el error 400
+    if (nombre.length < 3) {
+        return res.status(400).json({
+            error: "El nombre debe tener al menos 3 letras"
+        });
+    }
+    res.send(`Hola ${nombre}, bienvenido`);
+});
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
