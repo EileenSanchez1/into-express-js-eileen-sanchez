d22f5572f32db58417972fc49e98988996d95281
felipe122-cpu <eileensanchez49@gmail.com>
2026-08-06 16:13:40 -0500

    edicion app y creacion .env Ruta1 en la rama commonjs

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-import express from 'express'; // <--- Método ES (ESM)
+require('dotenv').config();
+
+const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
   res.send('Hola , estamos aprendiendo express con la ficha 3407184');
+});
+
+app.get("/Ruta1", (_, res) => {
+  res.json({
+    datos_personales: {
+      nombre: "Eileen",
+      apellido: "Sanchez",
+      ListaTelefono: ["3105563740", "3103191929"]
+    },
+    datos_programa: {
+      nombre: "Analisis y desarrollo de software",
+      tipo_programa: "Tecnologo",
+      ficha: 3407184
+    }
+  });
 });
 
 app.listen(port, () => {
