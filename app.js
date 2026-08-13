5f421db0acb2ccc09e3d774cc93720c3c5de3021
EileenSanchez1 <eileensanchez49@gmail.com>
2026-08-13 14:15:14 -0500

    Creacion lista de 3 aprendices endpoint

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-import express from 'express'; // <--- Método ES (ESM)
-
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (_, res) => {
-  res.send('Hola , estamos aprendiendo express con la ficha 3407184');
+app.get("/", function(req, res) {
+  res.send("API de aprendices,Endpoint principal ");
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+
+app.get("/aprendices", (req, res) => {
+  const aprendices = [
+    {
+      id: 1,
+      nombre: "Eileen Sanchez",
+      edad: 17,
+      correo: "eileensanchez49@gmail.com",
+      imgPerfil: "https://"
+    },
+    {
+      id: 2,
+      nombre: "Valeria Sandoval",
+      edad: 18,
+      correo: "valxsc03@gmail.com",
+      imgPerfil: "https://"
+    },
+    {
+      id: 3,
+      nombre: "Sofia Nieto",
+      edad: 19,
+      correo: "sofianieto1@gmail.com",
+      imgPerfil: "https://"
+    }
+  ];
+
+  res.json(aprendices);
+});
+
+
+app.listen(port, function() {
+  console.log(`SERVIDOR:http://localhost:${port}`);
 });
